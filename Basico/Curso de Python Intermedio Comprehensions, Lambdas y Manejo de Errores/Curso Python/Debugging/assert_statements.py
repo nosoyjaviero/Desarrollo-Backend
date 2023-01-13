8822eb3b8521d9734ee134aca5aa34343f22deb4
nosoyjaviero <lopez4935@gmail.com>
2023-01-13 00:40:57 -0600

    reto completado

--- Basico/Curso de Python Intermedio Comprehensions, Lambdas y Manejo de Errores/Curso Python/Debugging/assert_statements.py
+++ Basico/Curso de Python Intermedio Comprehensions, Lambdas y Manejo de Errores/Curso Python/Debugging/assert_statements.py
@@ -7,10 +7,23 @@ def divisors(num):
 
 
 def run():
-    num = input('Ingresa un número: ')
-    assert num.isnumeric(), "Debes ingresar un número"
-    print(divisors(int(num)))
-    print("Terminó mi programa")
+    
+    # num = input('Ingresa un número: ')
+    # assert num.isnumeric(), "Debes ingresar un número"
+    # print(divisors(int(num)))
+    # print("Terminó mi programa")
+    
+ # reto  
+     num=(input('Ingresa un número: '))
+    
+     assert num.lstrip('-').isdigit() , "Debes ingresar un número"
+    
+     assert int(num) >= 0, "Numero no debe ser negativo"    
+     print(divisors(int(num)))
+     print("Terminó mi programa")
+
+
+
 
 
 if __name__ == '__main__':
