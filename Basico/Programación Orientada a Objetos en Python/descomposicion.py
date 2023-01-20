f5908660a23d725e8540d06fda40dc37ce844082
nosoyjaviero <lopez4935@gmail.com>
2023-01-19 21:36:06 -0600

    Reto de descomposicion

--- Basico/Programación Orientada a Objetos en Python/descomposicion.py
+++ Basico/Programación Orientada a Objetos en Python/descomposicion.py
@@ -1,10 +1,13 @@
 class Automovil:
-     def __init__(self, modelo, marca, color):
+     def __init__(self, modelo, marca, color,año, pasajeros=1 ):
       self.modelo=modelo
       self. marca =  marca
       self.color= color
       self._estado='en_reposo'
       self._motor= Motor(cilindros=4)
+      self.pasajeros=pasajeros
+      self.chocado=False
+      self.año=año
       
      def acelerar (self, tipo='despacio'):
         if tipo=='despacio':
@@ -13,6 +16,13 @@ class Automovil:
             self._motor.inyecta_gasolina(3)
             
         self._estado='en_movimiento'
+        
+     def auto_chocado(self, chocado):
+         if chocado==True:
+             self.chocado=True
+        
+          
+        
 
 class Motor:
     def __init__(self,cilindros, tipo='gasolina'):
@@ -22,3 +32,20 @@ class Motor:
         
     def inyecta_gasolina(self,cantidad):
         pass
+    
+    
+        
+class llantas:
+    def __init__(self,tipo,marca,agujeros=0,ponchada=False):
+        self.tipo=tipo
+        self.marca=marca
+        self.agujeros=agujeros
+        self.ponchada=ponchada
+        
+    def llantaPonchada(self,ponchada):
+        if ponchada:
+            self.ponchada=True
+            print("Ir al mecanico a reparar")
+            
+    def AumentarAgujeros(self, agujeros):
+        self.agujeros=agujeros
