79d70a3de166d510b916a0dac31e568184190de8
nosoyjaviero <lopez4935@gmail.com>
2023-01-21 00:06:25 -0600

    mejora

--- Basico/Programación Orientada a Objetos en Python/3. Busqueda Lineal/busqueda_binaria.py
+++ Basico/Programación Orientada a Objetos en Python/3. Busqueda Lineal/busqueda_binaria.py
@@ -27,11 +27,11 @@ if __name__ == '__main__':
     tamano_de_lista = int(input('De que tamano es la lista? '))
     objetivo = int(input('Que numero quieres encontrar? '))
 
-    lista = sorted([random.randint(0, 100) for i in range(tamano_de_lista)])
+    lista = sorted([random.randint(0, tamano_de_lista) for i in range(tamano_de_lista)])
 
     encontrado = busqueda_binaria(lista, 0, len(lista), objetivo)
-    print(f'El numero de veces que se itero fue {contador}')
+    
     print(lista)
     print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
-    # print(math.log2(len(lista)))
+    print(f'El numero de veces que se itero fue {contador}')
    
