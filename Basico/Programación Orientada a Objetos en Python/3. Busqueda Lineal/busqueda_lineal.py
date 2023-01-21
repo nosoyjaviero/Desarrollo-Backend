050c73b50216b7d0e16061c1a86cf54c00d05a37
nosoyjaviero <lopez4935@gmail.com>
2023-01-21 00:11:00 -0600

    Reto busqueda lineal completado

--- Basico/Programación Orientada a Objetos en Python/3. Busqueda Lineal/busqueda_lineal.py
+++ Basico/Programación Orientada a Objetos en Python/3. Busqueda Lineal/busqueda_lineal.py
@@ -1,10 +1,12 @@
 import random
-
+contador=0
 def busqueda_lineal(lista, objetivo):
+    global contador
     match = False
 
     for elemento in lista: # O(n)
-        if elemento == objetivo:
+        contador+=1;
+        if elemento == objetivo:            
             match = True
             break
 
@@ -15,8 +17,9 @@ if __name__ == '__main__':
     tamano_de_lista = int(input('De que tamano sera la lista? '))
     objetivo = int(input('Que numero quieres encontrar? '))
 
-    lista = [random.randint(0, 100) for i in range(tamano_de_lista)]
+    lista = [random.randint(0, tamano_de_lista) for i in range(tamano_de_lista)]
 
     encontrado = busqueda_lineal(lista, objetivo)
     print(lista)
     print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+    print(f'El numero de veces que se itero fue {contador}')
